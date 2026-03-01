83e500084e9f2032cdd872f70bb71fb1b21e94df
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-02-28 19:57:13 -0500

    DB export

--- Backend/DB/CrearDByTablas.sql
+++ Backend/DB/CrearDByTablas.sql
@@ -1,20 +1,23 @@
--- 1. Crear la base de datos
-CREATE DATABASE dbVehiculo;
-USE dbVehiculo;
+DROP TABLE IF EXISTS `carga`;
+CREATE TABLE `carga` (
+  `id_carga` int NOT NULL AUTO_INCREMENT,
+  `peso` varchar(100) NOT NULL,
+  `destino` varchar(50) DEFAULT NULL,
+  `contenido` varchar(225) DEFAULT NULL,
+  `id_vehiculo` int DEFAULT NULL,
+  PRIMARY KEY (`id_carga`),
+  KEY `id_vehiculo` (`id_vehiculo`),
+  CONSTRAINT `carga_ibfk_1` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculo` (`id_vehiculo`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---tablas base 
-CREATE TABLE vehiculo (
-    id_vehiculo INT AUTO_INCREMENT PRIMARY KEY,
-    modelo VARCHAR(100) NOT NULL,
-    nro_identificacion VARCHAR(50) UNIQUE NOT NULL, 
-);
-CREATE TABLE carga(
-	id_carga INT AUTO_INCREMENT PRIMARY KEY,
-    peso VARCHAR(100) NOT NULL,
-    destino VARCHAR(50),
-    contenido VARCHAR(225),
-    id_vehiculo INT,    
-    -- Relación con la tabla vehiculo
-    FOREIGN KEY (id_vehiculo) REFERENCES vehiculo(id_vehiculo)
+INSERT INTO `carga` VALUES (1,'512','Liam','jdasda',NULL),(7,'512','otocons',NULL,NULL);
 
-);
+DROP TABLE IF EXISTS `vehiculo`;
+CREATE TABLE `vehiculo` (
+  `id_vehiculo` int NOT NULL AUTO_INCREMENT,
+  `modelo` varchar(100) NOT NULL,
+  `nro_identificacion` varchar(50) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_vehiculo`),
+  UNIQUE KEY `nro_identificacion` (`nro_identificacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
